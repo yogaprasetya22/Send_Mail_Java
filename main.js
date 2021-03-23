fbd841974bdaae13491d8dd725e66d92bba07424
yogaprasetya22 <yogaagle123z@gmail.com>
2021-03-23 21:58:19 +0700

    Belajar

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const form = document.querySelector('form');
+
 function send() {
   Email.send({
     Host: "smtp.gmail.com",
@@ -7,11 +9,12 @@ function send() {
     From: document.getElementById("email").value,
     Subject: document.querySelector("#name").value + " mengirim anda pesan",
     Body: document.getElementById("message").value,
-  }).then(function (message) {
-    Swal.fire({
-      title: document.getElementById("name").value,
-      text: "Terima Kasih",
-      icon: "success",
-    });
-  });
+  }).then(
+    (response) => form.reset(),
+      Swal.fire({
+        title: document.getElementById("name").value,
+        text: "Terima Kasih",
+        icon: "success",
+      })
+  );
 }
